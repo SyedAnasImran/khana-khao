9e75693aa5b619627f49e68743f29bcc3c4d072b
Anas <anas@example.com>
2023-10-16 23:06:34 +0500

    16 Oct

--- Models/User.js
+++ Models/User.js
@@ -1,20 +1,36 @@
 const connectDb = require("../connectDb");
 
 let User = {
-  //CHECK USER EXISTS
+  //QUERIES
+  insert_query: `Insert into users(email,password,first_name,last_name) values(:1,:2,:3,:4)`,
+  find_query: `SELECT * FROM USERS where EMAIL=:1`,
 
-  //INSERT USER
-  insertUser: function (data) {
+  //FUNCTIONS
+  signUp: function (res, data) {
+    console.log(data);
     connectDb().then(async (con) => {
-      const res = await con.executeMany(
-        `Insert into users(email,password,first_name,last_name) values(:1,:2,:3,:4)`,
-        data
-      );
-      console.log("User Inserted");
-      con.commit();
-      con.close();
+      let result = await con.execute(this.find_query, [data[0]]);
+      result = result.rows;
+      if (!result.length) {
+        await con.execute(this.insert_query, data); // USER CREATED
+        con.commit();
+        res.send("User Created");
+      } else {
+        res.send("User Exists");
+      }
     });
   },
 };
 
 module.exports = User;
+
+// const res = await con.executeMany(this.insertQuery, data);
+//       console.log("User Inserted");
+//       const res = await con.executeMany(
+//         `SELECT * FROM USERS where EMAIL=:ali@email.com`,
+//         data
+//       );
+//       con.close();
+//       res = res.rows;
+//       if (!res.length) return false;
+//       return true;
--- Routes/signup.js
+++ Routes/signup.js
@@ -2,10 +2,10 @@ const express = require("express");
 const User = require("../Models/User.js");
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const data = req.body;
-  const user = [Object.values(data)]; // converted user object to array
-  User.insertUser(user);
+  const user = Object.values(data); // converted user object to array
+  User.signUp(res, user);
 });
 
 module.exports = router;
